4405044ed41d36fba06dae8678a171e4143c9271
pantharshit007 <aaryanpandit3527@gmail.com>
2024-11-23 17:28:58 +0530

    🛠️ Fix: AppBar spacing

--- apps/user-app/app/(sidebar)/layout.tsx
+++ apps/user-app/app/(sidebar)/layout.tsx
@@ -14,9 +14,7 @@ async function SideBarLayout({ children }: { children: React.ReactNode }) {
           <SidebarInset>
             <div className="flex-1 h-full flex-col">
               <CollapsableMobile />
-              <main className=" w-full max-lg:max-w-[1200px] max-w-maxContent mx-auto">
-                {children}
-              </main>
+              <main className=" w-full max-lg:max-w-[1200px] max-w-maxContent ">{children}</main>
             </div>
           </SidebarInset>
         </SidebarProvider>
